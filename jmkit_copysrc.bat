a0ca454e473bf66e576d4339e61ce28ab9c38552
toannguyen <toan.nguyencanh89@gmail.com>
2014-10-27 23:07:58 +0700

    optimize build jmkits

--- jmkit_copysrc.bat
+++ jmkit_copysrc.bat
@@ -1,4 +1,8 @@
 @echo off
+if "%JMKIT_DIR%"=="" goto :end
+if not exist %JMKIT_DIR% goto :end
+
+
 if exist %JMKIT_DIR%\Android-JMKit\src RMDIR %JMKIT_DIR%\Android-JMKit\src\ /S /Q
 MKDIR %JMKIT_DIR%\Android-JMKit\src\
 echo -------------------UPDATE JMKIT SOURCES-------------------
@@ -6,4 +10,5 @@ echo XCOPY %JMKIT_DIR%\source\common\* %JMKIT_DIR%\Android-JMKit\src\* /e
 XCOPY %JMKIT_DIR%\source\common\* %JMKIT_DIR%\Android-JMKit\src\* /e
 XCOPY %JMKIT_DIR%\source\android\* %JMKIT_DIR%\Android-JMKit\src\* /e
 echo JMKIT SOURCES HAVE BEEN UPDATED
+:end
 CD %ROOT%
