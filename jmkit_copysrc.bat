fcfcce2e2574ae666eea40846f5e140b6f9818de
toannguyen <toan.nguyencanh89@gmail.com>
2015-01-27 14:58:40 +0700

    rename jmkits projects

--- jmkit_copysrc.bat
+++ jmkit_copysrc.bat
@@ -3,12 +3,12 @@ if "%JMKIT_DIR%"=="" goto :end
 if not exist %JMKIT_DIR% goto :end
 
 
-if exist %JMKIT_DIR%\Android-JMKit\src RMDIR %JMKIT_DIR%\Android-JMKit\src\ /S /Q
-MKDIR %JMKIT_DIR%\Android-JMKit\src\
+if exist %JMKIT_DIR%\android-jmkits\src RMDIR %JMKIT_DIR%\android-jmkits\src\ /S /Q
+MKDIR %JMKIT_DIR%\android-jmkits\src\
 echo -------------------UPDATE JMKIT SOURCES-------------------
 echo XCOPY %JMKIT_DIR%\source\common\* %JMKIT_DIR%\Android-JMKit\src\* /e
-XCOPY %JMKIT_DIR%\source\common\* %JMKIT_DIR%\Android-JMKit\src\* /e
-XCOPY %JMKIT_DIR%\source\android\* %JMKIT_DIR%\Android-JMKit\src\* /e
+XCOPY %JMKIT_DIR%\source\common\* %JMKIT_DIR%\android-jmkits\src\* /e
+XCOPY %JMKIT_DIR%\source\android\* %JMKIT_DIR%\android-jmkits\src\* /e
 echo JMKIT SOURCES HAVE BEEN UPDATED
 :end
 CD %ROOT%
